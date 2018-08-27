08c8acf328ab87fcf606e91ec703d23faae22996
akshaybhatia10 <akshaybhatia10@gmail.com>
2018-08-27 21:18:22 +0530

    Fix U-Net upsample

---
+++ model.py
@@ -0,0 +1,70 @@
+import sys
+sys.path.append('./fastai')
+from fastai.conv_learner import *
+from fastai.dataset import *
+import torch
+from helper import SaveFeatures
+
+f = resnet34
+cut, lr_cut = model_meta[f]
+
+class StdUpsample(nn.Module):
+    def __init__(self, nin, nout):
+        super().__init__()
+        self.conv = nn.ConvTranspose2d(nin, nout, 2, stride=2)
+        self.bn = nn.BatchNorm2d(nout)
+        
+    def forward(self, x): 
+        return self.bn(F.relu(self.conv(x)))
+
+class UnetBlock(nn.Module):
+    def __init__(self, up_in, x_in, n_out):
+        super().__init__()
+        up_out = x_out = n_out//2
+        self.x_conv  = nn.Conv2d(x_in,  x_out,  1)
+        self.tr_conv = nn.ConvTranspose2d(up_in, up_out, 2, stride=2)
+        self.bn = nn.BatchNorm2d(n_out)
+        
+    def forward(self, up_p, x_p):
+        up_p = self.tr_conv(up_p)
+        x_p = self.x_conv(x_p)
+        cat_p = torch.cat([up_p,x_p], dim=1)
+        return self.bn(F.relu(cat_p))
+
+class Unet34(nn.Module):
+    def __init__(self, rn):
+        super().__init__()
+        self.rn = rn
+        self.sfs = [SaveFeatures(rn[i]) for i in [2,4,5,6]]
+        self.up1 = UnetBlock(512,256,256)
+        self.up2 = UnetBlock(256,128,256)
+        self.up3 = UnetBlock(256,64,256)
+        self.up4 = UnetBlock(256,64,256)
+        self.up5 = nn.ConvTranspose2d(256, 1, 2, stride=2)
+        
+    def forward(self,x):
+        x = F.relu(self.rn(x))
+        x = self.up1(x, self.sfs[3].features)
+        x = self.up2(x, self.sfs[2].features)
+        x = self.up3(x, self.sfs[1].features)
+        x = self.up4(x, self.sfs[0].features)
+        x = self.up5(x)
+        return x
+
+class UnetModel():
+    def __init__(self, model, name='unet'):
+        self.model = model
+        self.name = name
+
+    def get_layer_groups(self, precompute):
+        lgs = list(split_by_idxs(children(self.model.rn), [lr_cut]))
+        return lgs + [children(self.model)[1:]]
+
+
+class UpsampleModel():
+    def __init__(self,model,name='upsample'):
+        self.model,self.name = model,name
+
+    def get_layer_groups(self, precompute):
+        lgs = list(split_by_idxs(children(self.model.rn), [lr_cut]))
+        return lgs + [children(self.model)[1:]]
