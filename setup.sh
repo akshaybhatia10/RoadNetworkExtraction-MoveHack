e8ecf9505a36d122c7079b442c974a89b642647e
Akshay Bhatia <akshaybhatia10@gmail.com>
2018-08-28 12:16:47 +0530

    minor fix

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+conda install -c intel openmp 
+conda install pytorch-cpu torchvision-cpu -c pytorch
 git clone https://github.com/fastai/fastai.git
 cd fastai
 conda env update -f environment-cpu.yml
